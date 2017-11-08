2d231606eadaa0bbb256087bf21828510ecd8ec3
Winston Riley <masonwr@mac.com>
2017-11-08 09:39:39 -0700

    adds further comments

--- client/Main.hs
+++ client/Main.hs
@@ -2,12 +2,13 @@ import Network.Socket
 import System.IO
 import System.Environment
 import Conf (defaultPort)
-import TTTParser
 import NetworkTTT
 import TicTacTow
 import System.Random
 import Data.Maybe
 
+
+-- Basic Configuration information
 clientPlayer :: Player
 clientPlayer = X
 
@@ -16,10 +17,12 @@ localHostIP :: String
 localHostIP = "127.0.0.1"
 
 
+
+-- Main loop
 main :: IO ()
 main = do
+  -- if no host ip address is specified, local host is assumed.
   args <- getArgs
-
   let ipAddr = (head $ args ++ [localHostIP])
   
   putStrLn $ "Connecting too: " ++ ipAddr  
@@ -30,18 +33,19 @@ main = do
   play handle (gridToNG startGrid)
 
 
+-- get a random starting position for the client player.
 getRandomStartingGrid :: IO Grid
 getRandomStartingGrid = do
   startPlayI <- randomRIO (0, 8 :: Int)
   pure . fromJust $ move empty startPlayI clientPlayer
 
 
+-- Play loop. If there is an message print to screen,
+-- otherwise there is a game, make a play.
 play :: Handle -> NetworkGame -> IO ()
 play handle game = do
-  putStrLn $ "play...?"
+  putStrLn  "play...?"
   putGrid' game
-
-  --_ <- getLine
   
   postMove handle game  
   gridE <- getGrid handle
@@ -51,14 +55,18 @@ play handle game = do
     Right grid -> play handle (makeMove grid clientPlayer)
 
 
+-- Create a socket and wrap it in a IO Handle, a more general IO interface.
 makeHandle :: String -> IO Handle
 makeHandle ipStr = do
-  sock <- socket AF_INET Stream 0
-  localHost <- inet_addr ipStr
+  
+  sock <- socket AF_INET Stream 0 -- create a IPv4 TCP socket.  
+  localHost <- inet_addr ipStr    -- case ip string into addr
 
+  -- build server networking address, and connect the socket to it.
   let serverAddr = SockAddrInet defaultPort localHost
   connect sock serverAddr
 
+  -- wrap socket in handle, and set buffering rule.
   handle <- socketToHandle sock ReadWriteMode
   hSetBuffering handle LineBuffering
 
--- server/Main.hs
+++ server/Main.hs
@@ -8,38 +8,44 @@ import TicTacTow (putGrid, bestmove, Player(..), Grid)
 import TTTParser (parseGame, gridToStr)
 import NetworkTTT
 
+
+
+serverPlayer :: Player
 serverPlayer = O
 
-
+-- Main 
 main :: IO ()
 main = do
   putStrLn "hello from server!"
   runServer defaultPort
 
 
-
-stripnr :: String -> String
-stripnr = filter (/= '\r') . filter (/= '\n')
-
-
-
+-- Set up listening socket and run game loop
 runServer :: PortNumber -> IO ()
 runServer port = do
+  -- create a TCP IPv4 socket
   sock <- socket AF_INET Stream 0
   setSocketOption sock ReuseAddr 1
+
+  -- bind socket to port (707)
   bind sock (SockAddrInet port iNADDR_ANY)
   listen sock 2
+  
   mainLoop sock
 
 
 
 mainLoop :: Socket -> IO ()
 mainLoop sock = do
+  -- wait for connections
   conn <- accept sock
+
+  -- fork new IO thread to handle connection
   _ <- forkIO (runConn conn)   
   mainLoop sock
 
 
+-- handle the newly created connection, hand off to gameloop
 runConn :: (Socket, SockAddr) -> IO ()
 runConn (sock, addr) = do
   print $ "Connection from " ++ (show addr)
@@ -48,16 +54,17 @@ runConn (sock, addr) = do
   runGame handle
 
 
+-- Run the actual game, with some logging turned on.
 runGame :: Handle -> IO ()
 runGame handle = do
   netGame <- getGrid handle
-
   putStrLn $ "received: " ++ show netGame
   
   case netGame of
     Left err   -> do
       putStrLn "closing connection."
       hPutStrLn handle err
+      
     Right grid -> do
       putStrLn $ "sending: " ++ show (makeMove grid serverPlayer)
       postMove handle (makeMove grid serverPlayer)
